f113a2b3d194a9f50d41a7e0380bd4d6b5531a80
Favio Valero <favio102@protonmail.com>
2024-03-10 07:12:27 -0700

    api .env added

--- src/api/tv-shows.js
+++ src/api/tv-shows.js
@@ -1,13 +1,49 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.themoviedb.org/3/tv/popular";
-const API_KEYS_PARAM =
-  "api_key=6f1ce0e9b12eed130f12fdab778c6bf2";
+// const BASE_URL = "https://api.themoviedb.org/3/";
+// const API_KEYS_PARAM =
+//   "api_key=dsadasdasdas";
+
+const APIKey = process.env.REACT_APP_API_KEY
+const APIToken = process.env.REACT_APP_TOKEN
+
 
 export class TVShowAPI {
   static async fetchPopulars() {
-    const response = await axios.get(`${BASE_URL}tv/popular${API_KEYS_PARAM}`);
-    console.log(response.data.results);
-    return response.data.results;
+    const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/tv/popular",
+      params: { language: "en-US", page: "1", api_key: APIKey },
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${APIToken}`,
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+      console.log(response.data.results);
+      return response.data.results;
+    } catch (error) {
+      console.error(error);
+    }
   }
 }
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://api.themoviedb.org/3/tv/popular',
+//   params: {language: 'en-US', page: '1'},
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer asdasdasd'},
+// };
+
+// axios
+//   .request(options)
+//   .then(function (response) {
+//     console.log(response.data);
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
